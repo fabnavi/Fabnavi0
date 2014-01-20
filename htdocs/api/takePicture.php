53db4dc602e2a2fa9a9d6f45e6a421b157e12645
hrl7 <hrlclb@gmail.com>
2014-01-20 21:08:28 +0900

    fixed for 1/23

--- htdocs/api/takePicture.php
+++ htdocs/api/takePicture.php
@@ -1,6 +1,6 @@
 <?php
   $project_id = $_GET["project_id"];
-
+  error_reporting(E_ERROR);
   $dataDirectory = getenv("DATA_DIRECTORY");
   $directory = "../".$dataDirectory."/".$project_id;
 
@@ -30,18 +30,19 @@
     $x = intval(getenv("PHOTO_CROP_X"));
     $y = intval(getenv("PHOTO_CROP_Y"));
     $source = imagecreatefromjpeg($originalFilePath);
-    $rotated = imagerotate($source, $degree, 0);
+    $rotated = imagerotate($source, 179.5, 1);
     $image = imagecreatetruecolor($width, $height);
     //imagecopyresized($image, $rotated, 0, 0, $x, $y, $width, $height, $width, $height);
     dbg($x.":".$y." ,  ".$width.":".$height);
-    imagecopyresized($image,$rotated, 0, 0, 0, 0, $width, $height, $width, $height);
+    imagecopyresized($image,$rotated, 0, 0, 539, 558, $width, $height, 670, 435);
     imagejpeg($image, $localPath);
 
     imagedestroy($source);
     imagedestroy($rotated);
     imagedestroy($image);
   } catch (Exception $e) {
-    $result["error"] = $e->getMessage();
+    //$result["error"] = $e->getMessage();
+
   }
 
   echo json_encode($result);
--- htdocs/js/record.js
+++ htdocs/js/record.js
@@ -3,7 +3,34 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 var RecordController = {
   init: function() {
-    $("#start").click(RecordController.start);
+      $("#start").click(function(){
+        RecordController.start();
+        $('#take').hide();
+      });
+      $(window).keydown(function(e){
+        console.log(e.keyCode);
+        switch(e.keyCode){
+          case 101:
+            if($('#start').is(':visible')){
+              RecordController.start();
+              $('#take').hide();
+            } else {
+              RecordController.take();
+            }
+            break;
+          case 104:
+            if($('li').is(':visible')){
+              $('li').hide();
+              $('#take').hide();      
+            } else { 
+              $('li').show();
+              $('#take').show();      
+            }
+            break;
+          default:
+            break;
+        }
+      });
   },
 
   start: function() {
@@ -14,13 +41,17 @@ var RecordController = {
         return;
       }
       RecordController.project_id = result["id"];
-      $("#take").show();
+//      $("#take").show();
       $("#take").click(RecordController.take);
-      $(window).keydown(RecordController.take);
+
+              
+       //       RecordController.take);
     });
   },
 
   take: function() {
+    $('#take').hide();
+    $('li').hide();
     clearTimeout(RecordController.timer);
     RecordController.timer = setTimeout(function() {
       CommonController.getJSON("/api/takePicture.php?project_id="+RecordController.project_id, function(result, error) {
@@ -32,6 +63,7 @@ var RecordController = {
         var img = $(document.createElement("img"));
         img.attr("src", result["url"]);
         li.append(img);
+        li.hide();
         $("#processes").append(li);
       });
     }, 10);
