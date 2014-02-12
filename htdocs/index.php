cfd588b4b8945829c2974788a7768aea4634e181
hrl7 <hrlclb@gmail.com>
2014-02-12 17:07:04 +0900

    add index.php

---
+++ htdocs/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title></title>
+</head>
+<body>
+  <h1>Fabnavi</h1>
+  <div>
+    <ul>
+        <li>
+          <a href="http://fabnavi.local/play.php">Make!</a>
+        </li>    
+    
+        <li>
+          <a href="http://fabnavi.local/record.php"> Record Making</a>
+        </li>
+    </ul>
+
+  </div>
+</body>
+</html>
