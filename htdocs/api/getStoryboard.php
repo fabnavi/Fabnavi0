f688f00ca52fe3e5887f673f8ae2b2c8f9f41b8d
hrl7 <hrlclb@gmail.com>
2014-02-19 19:48:21 +0900

    add getStoryboard.php

---
+++ htdocs/api/getStoryboard.php
@@ -0,0 +1,29 @@
+<?php
+  $project_id = $_GET["project_id"];
+  $result = array();
+  try {
+    $dataDirectoryRelational = getenv("DATA_DIRECTORY");
+    $dataDirectory = "../".$dataDirectoryRelational;
+    $dataDirectoryRef = opendir($dataDirectory);
+    $projectDirectory = $dataDirectory.$project_id;
+    $projectDirectoryRef = opendir($projectDirectory);
+    while($imageFile = readdir($projectDirectoryRef)) {
+//      if (strpos($imageFile, ".") === 0) {
+//        continue;
+//      }
+      if (!preg_match("/[.](jpe?g)|(png)$/i", $imageFile)) {
+        continue;
+      }
+      if (is_dir($projectDirectory."/".$imageFile) == true) {
+        continue;
+      }
+      $photoURL = $dataDirectoryRelational.$project_id."/".$imageFile;
+      array_push($result, $photoURL);
+    }
+    closedir($projectDirectoryRef);
+
+  } catch (Exception $e) {
+    $result["error"] = $e->getMessage();
+  }
+  echo json_encode($result);
+?>
