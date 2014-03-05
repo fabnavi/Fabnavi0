e2c714387a605361651d3c1fc9e6ad43fce798a5
hrl7 <hrlclb@gmail.com>
2014-03-05 16:55:57 +0900

    Fix Play.php

--- htdocs/play.php
+++ htdocs/play.php
@@ -21,7 +21,7 @@
 
     <meta name="viewport" content="width=device-width, user-scalable=no">
   </head>
-  <body>
+  <body onload="PlayController.init()">
     <?php include('header.php.inc'); ?>
     <div id="contents">
       <ul id="project-list"></ul>
